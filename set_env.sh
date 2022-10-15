a40b62e4cc2a23c898d688dc63e6fe1758e9a295
vianneyKB <36236520+vianneyKB@users.noreply.github.com>
2022-10-15 15:22:12 +0200

    all

---
+++ set_env.sh
@@ -0,0 +1,11 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=p3_db_vianney
+export POSTGRES_PASSWORD=P3.vny007
+export POSTGRES_HOST=database-p3.ccdvsifcinqw.us-east-1.rds.amazonaws.com
+export POSTGRES_DB=postgres
+export AWS_BUCKET=s3-vianney-p3
+export AWS_REGION=us-east-1
+export AWS_PROFILE=p3-vianney-dev
+export JWT_SECRET=testing
+export URL=http://localhost:8100
