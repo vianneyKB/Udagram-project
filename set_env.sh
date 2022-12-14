530eaa91fa9febdab3c2f92f2ca144dea63572da
vianneyKB <36236520+vianneyKB@users.noreply.github.com>
2022-12-15 01:10:22 +0200

    ci/cd done

--- set_env.sh
+++ set_env.sh
@@ -2,10 +2,11 @@
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
 export POSTGRES_USERNAME=p3_db_vianney
 export POSTGRES_PASSWORD=P3.vny007
-export POSTGRES_HOST=database-p3.ccdvsifcinqw.us-east-1.rds.amazonaws.com
+export POSTGRES_HOST=p3-db.cwl2x5iqaxlj.us-east-1.rds.amazonaws.com
 export POSTGRES_DB=postgres
 export AWS_BUCKET=s3-vianney-p3
 export AWS_REGION=us-east-1
-export AWS_PROFILE=p3-vianney-dev
+export AWS_PROFILE=todouser
 export JWT_SECRET=testing
 export URL=http://localhost:8100
+# eksctl create cluster --name my-cluster --region region-code --fargate
